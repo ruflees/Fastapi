b62335322f8277f7c074bffd9ae03a9a8eef6438
ruflees <humphreymurbach@gmail.com>
2023-06-07 16:00:06 -0300

    git

---
+++ app/schemas.py
@@ -0,0 +1,57 @@
+from typing import Optional
+from pydantic import BaseModel, EmailStr
+from datetime import datetime
+from pydantic.types import conint
+
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class UserOut(BaseModel):
+    id: int
+    email: EmailStr
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
+
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    id: Optional[str] = None
+
+
+class PostBase(BaseModel):
+    title: str
+    content: str
+
+
+class PostCreate(PostBase):
+    pass
+
+
+class PostResponse(PostBase):
+    id: int
+    created_at: datetime
+    edited: bool
+    owner_id: int
+    owner: UserOut
+
+    class Config:
+        orm_mode = True
+
+
+class Like(BaseModel):
+    post_id: int
+    dir: conint(ge=0,le=1)
